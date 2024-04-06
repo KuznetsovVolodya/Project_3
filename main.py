6be1ff6f8d13377de5d6f7ef700f4d190c5ed3f3
Volodya <kuznetsov.vk@bk.ru>
2024-04-06 15:07:14 +0300

    base_form

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def news():
+    return render_template('base.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
