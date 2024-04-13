671b302fb33d9777b81dab320136f1a11741ea89
Volodya <kuznetsov.vk@bk.ru>
2024-04-13 11:57:48 +0300

    generate_page_without_pymorphy

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('main.html')
+
+
+@app.route('/gen', methods=['POST', 'GET'])
+def generate():
+    gen_text = ""
+    if request.method == 'POST':
+        gen_text = request.form['about'] + 'a'
+    return render_template('gen.html', text=gen_text)
 
 
 if __name__ == '__main__':
