645637e30d11ae415bcfd75f2123a9bbd72bc665
Volodya <kuznetsov.vk@bk.ru>
2024-04-13 21:16:20 +0300

    generate_page_var_1

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, url_for, render_template, request
+from random import choice
+from text_creator import a
 
 app = Flask(__name__)
+
+
+
+
+def text_creator(o_t):
+    text = o_t
+    if len(text) >= 1:
+        for i in range(10):
+            if text.split()[-1] in a:
+                text += ' ' + choice(a[text.split()[-1]])
+            else:
+                text += ' ' + choice(list(a.keys()))
+    return text
 
 
 @app.route('/')
@@ -13,8 +28,8 @@ def index():
 def generate():
     gen_text = ""
     if request.method == 'POST':
-        gen_text = request.form['about'] + 'a'
-    return render_template('gen.html', text=gen_text)
+        gen_text = request.form['about']
+    return render_template('gen.html', text=text_creator(gen_text))
 
 
 if __name__ == '__main__':
---
+++ text_creator.py
@@ -0,0 +1,13 @@
+f = open('orig_text.txt', mode='r')
+data = f.read().split()
+for i in range(len(data)):
+    if not (data[i][-1].isalpha() or data[i][-1].isdigit()):
+        if len(data[i][:-1]) >= 1:
+            data[i] = data[i][:-1].lower()
+    else:
+        data[i] = data[i].lower()
+ans = set(data)
+a = {}
+for elem in ans:
+    indices = [data[i + 1] for i in range(len(data) - 1) if data[i] == elem]
+    a[elem] = indices
