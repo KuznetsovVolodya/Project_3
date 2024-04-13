3c11631ca39597278e3b2366a2a58ab2c32d3223
Volodya <kuznetsov.vk@bk.ru>
2024-04-13 11:15:24 +0300

    base_page

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
+
+@app.route('/')
 @app.route('/index')
-def news():
-    return render_template('base.html')
-
-
+def index():
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
